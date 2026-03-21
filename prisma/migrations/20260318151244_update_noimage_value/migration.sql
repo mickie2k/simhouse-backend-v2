f4e1be2a8e87928e6a83a5a824c3af2a6c479588
mickie2k <tpookongmek@gmail.com>
2026-03-21 15:35:10 +0700

    feat: update image default value

---
+++ prisma/migrations/20260318151244_update_noimage_value/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "simulatorlist" ALTER COLUMN "firstimage" SET DEFAULT 'noimage.webp',
+ALTER COLUMN "secondimage" SET DEFAULT 'noimage.webp',
+ALTER COLUMN "thirdimage" SET DEFAULT 'noimage.webp';
