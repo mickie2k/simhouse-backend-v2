64ca1987ea1e4454f319b8eebc5a2a1a2957ff99
mickie2k <tpookongmek@gmail.com>
2026-03-28 16:54:07 +0700

    feat: import module to booking

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { ReviewModule } from '../review/review.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { StorageModule } from 'src/storage/storage.module';
 
 @Module({
-    imports: [ReviewModule],
+    imports: [ReviewModule, PrismaModule, StorageModule],
     controllers: [BookingController],
     providers: [BookingService],
 })
